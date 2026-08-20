c1d658a1125266dd38d0aca4f6fe9e6eeccacf34
MansiKadvani <mrk912007@gmail.com>
2026-08-20 15:49:48 +0530

    Pull specific files from student_authentication branch

---
+++ SettleMate/Data/PasswordHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SettleMate.Data
+{
+    public class PasswordHelper
+    {
+        // Convert password into hashed text
+        public static string HashPassword(string password)
+        {
+            try
+            {
+                // Create SHA256 object
+                using SHA256 sha256 =
+                    SHA256.Create();
+
+                // Convert password into bytes
+                byte[] passwordBytes =
+                    Encoding.UTF8.GetBytes(password);
+
+                // Create hashed password bytes
+                byte[] hashBytes =
+                    sha256.ComputeHash(passwordBytes);
+
+                // Convert hash bytes into text
+                return Convert.ToBase64String(hashBytes);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
